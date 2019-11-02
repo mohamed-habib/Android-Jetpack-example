c8c40a71b1272cf0b7853db7f33884caff19fcfb
mohamed_habib <mohamed_habib@rayacorp.com>
2019-11-02 12:43:56 +0200

    deleted unused files

--- Mobiquity Task/app/src/main/java/com/android/mohamed_habib/ui/base/NavigationCommand.kt
+++
@@ -1,9 +0,0 @@
-package com.android.mohamed_habib.ui.base
-
-import androidx.navigation.NavDirections
-
-sealed class NavigationCommand {
-    data class To(val directions: NavDirections) : NavigationCommand()
-    object Back : NavigationCommand()
-    data class BackTo(val destinationId: Int) : NavigationCommand()
-}
